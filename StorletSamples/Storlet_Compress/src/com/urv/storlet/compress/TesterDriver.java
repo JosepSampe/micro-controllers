3c61bab65917c8bf3fdd8c44c6779dd33133ac11
JosepSampe <jsampe@gmail.com>
2016-03-07 17:14:10 +0100

    Added information to the UbuntuOne trace Storlet

---
+++ StorletSamples/Storlet_Compress/src/com/urv/storlet/compress/TesterDriver.java
@@ -0,0 +1,61 @@
+package com.urv.storlet.compress;
+
+import java.io.*;
+
+import com.ibm.storlet.common.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class TesterDriver {
+	
+	public static final String INPUT_FILE_NAME = "input/paper.gz";
+	public static final String OUTPUT_FILE_NAME = "input/paper.pdf";
+	public static final String OUTPUT_MD_FILE_NAME = "input/output_md.txt";
+	public static final String LOGGER_FILE_NAME = "input/logger";	
+	
+	public static void main(String[] args) {
+		System.out.println("entering main");
+		try {
+
+			FileInputStream infile = new FileInputStream(INPUT_FILE_NAME);
+			FileOutputStream outfile = new FileOutputStream(OUTPUT_FILE_NAME);
+			FileOutputStream outfile_md = new FileOutputStream(OUTPUT_MD_FILE_NAME);
+
+			HashMap<String, String> md = new HashMap<String, String>();
+			StorletInputStream inputStream1 = new StorletInputStream(infile.getFD(), md);
+	        StorletObjectOutputStream outStream = new StorletObjectOutputStream(outfile.getFD(), md, outfile_md.getFD());
+	        
+	        ArrayList<StorletInputStream> inputStreams = new ArrayList<StorletInputStream>();
+	        inputStreams.add(inputStream1);	        
+	        ArrayList<StorletOutputStream> outStreams = new ArrayList<StorletOutputStream>();
+	        outStreams.add(outStream);
+	        
+			FileOutputStream loggerFile = new FileOutputStream(LOGGER_FILE_NAME);	
+			CompressStorlet storlet = new CompressStorlet();	
+			StorletLogger logger = new StorletLogger(loggerFile.getFD());				
+			Map<String, String> parameters = new HashMap<String, String>();	
+
+			parameters.put("compression","gz");
+			parameters.put("reverse","True");
+			System.out.println("before storlet");
+			storlet.invoke(inputStreams, outStreams, parameters, logger);
+			System.out.println("after storlet");
+			
+			infile.close();
+			outfile.close();
+			outfile_md.close();
+			
+			loggerFile.close();
+			
+		}
+		catch (Exception e) {
+			System.out.println("had an exception");
+			System.out.println(e.getMessage());
+		}
+		System.out.println("exiting main");
+	}
+
+}
+
--- StorletSamples/Storlet_UOneTrace/src/com/urv/storlet/uonetrace/UOneTraceStorlet.java
+++ StorletSamples/Storlet_UOneTrace/src/com/urv/storlet/uonetrace/UOneTraceStorlet.java
@@ -34,6 +34,8 @@ public class UOneTraceStorlet implements IStorlet {
 
     private void loadHeader()
     {
+    	
+    	// Ad-hoc headers: only works with ubuntu-one trace.
         String header[] = {
             "t", "addr", "caps", "client_metadata", "current_gen", "ext", "failed", "free_bytes", "from_gen", "hash", 
             "level", "logfile_id", "method", "mime", "msg", "node_id", "nodes", "pid", "req_id", "req_t", 
@@ -84,7 +86,7 @@ public class UOneTraceStorlet implements IStorlet {
         
         // PARSING WHERE PARAMETERS
         if (where_string != null){
-	        logger.emitLog("Parsing where parameters");
+	        //logger.emitLog("Parsing where parameters");
 	        where = where_string.replaceAll("\\s+", "").split(" and ");
         }
                
@@ -93,14 +95,8 @@ public class UOneTraceStorlet implements IStorlet {
 		StorletObjectOutputStream storletObjectOutputStream = (StorletObjectOutputStream)outStreams.get(0);
 		storletObjectOutputStream.setMetadata(metadata);
 		OutputStream outputStream = storletObjectOutputStream.getStream();
-		
-
-		//logger.emitLog(new Date().toString()+" + 1");
-		//long comp_a, total, comp_b, comp=0;
-		//long io_b, io_a, totalio, io=0;
 				
         try {
-
 
         	BufferedReader br = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"));
             logger.emitLog("Going to read Input File");
@@ -114,44 +110,38 @@ public class UOneTraceStorlet implements IStorlet {
 			ByteBuffer bbuf = ByteBuffer.allocate(bufferLength);
             
             while ((line = br.readLine()) != null) {
-            	//comp_b = System.nanoTime();
             	valid = true;
 
-                //logger.emitLog(line);
-                // SEARCH FROZENSET                
+                // SEARCH FROZENSET (Only for ubuntu-one trace)               
         		if (line.indexOf("frozenset")>-1){
         			s = line.substring(line.indexOf("\"frozenset") + 1);
         			s = s.substring(0, s.indexOf("\""));
         			line = line.replace(s, "");
-                    //logger.emitLog("FrozenSet Found");
-                    //logger.emitLog(line);
         		}
                 
-                trace_line = line.split(","); // converts line to vector // ESTA LIMITANDO EL TRHOUGHPUT
+        		// convert line to vector
+                trace_line = line.split(",");
+                // After measure the performance of the Storlet I found that split function limits its performance.
+                // Split() is the most expensive operation in each iteration, maybe other technique (String tokenizer 
+                // or other) is better here.
                 
                 //logger.emitLog(Integer.toString(trace_line.length));
                 
                 if (trace_line.length < header_.size()) continue;
 
                 if ( where_string != null ){ //TODO: IMPLEMENT OR CLAUSE
-	                // CHECK WHERE CLAUSE
+	                // Check where clause
 	                for(k = 0; k < where.length; k++){
 	                    where_condition = where[k].split("=");
-	                    if(!trace_line[header_.get(where_condition[0])].equals(where_condition[1])){ // IF LINE DON'T MATCH WITH WHERE CLAUSE -> LINE NOT VALID
+	                    // IF LINE DON'T MATCH WITH WHERE CLAUSE -> LINE NOT VALID
+	                    if(!trace_line[header_.get(where_condition[0])].equals(where_condition[1])){
 	                    	valid = false;
 	                        break;
 	                    }
 	                }
                 }
-                
-                //comp_a = System.nanoTime();				
-				//total = comp_a-comp_b;	
-				//comp=comp+total;
-				
-				
-                //io_b = System.nanoTime();
-                // FINALLY IF LINE IS VALID -> RETURN IT
 
+                // FINALLY IF LINE IS VALID -> RETURN IT
                 out_line = "";
             	if(valid){
                 	if ( select_string == null || select[0].equals("*")){
@@ -165,22 +155,21 @@ public class UOneTraceStorlet implements IStorlet {
 	                    	}
 	                    }
 	                    out_line = out_line+"\n";
+	                    // Push line into the buffer:  Analyzing the performance of the Storlet I found that
+	                	// send the data line by line with "outputStream.write(out_line.getBytes())" is an
+	                	// expensive operation, so is better (and overall fastest) save the results in an
+	                    // intermediate buffer.
 	                    bbuf.put(out_line.getBytes());
 	                }
                 	
+                	// We only return the data when the buffer is almost full.
 	            	if (bbuf.remaining() < 100){
 	            		outputStream.write(bbuf.array());
 	            		bbuf.clear();
 	            	}    	
                 }
-                
-				//io_a = System.nanoTime();				
-				//totalio = io_a-io_b;	
-				//io=io+totalio;
             }
-            
-			//logger.emitLog("IO [ms]: "+((io)/1000000L));
-			//logger.emitLog("CPU [ms]: "+((comp)/1000000L));
+
 			
 	        br.close();
 	        outputStream.close();
