6e204f0f08ff9482f21c0d4ba411ecb1ffd8d79b
JosepSampe <josep.sampe@gmail.com>
2019-08-29 12:07:37 +0200

    Fixed redis lua script

--- Engine/swift/vertigo_middleware/handlers/proxy.py
+++ Engine/swift/vertigo_middleware/handlers/proxy.py
@@ -30,7 +30,7 @@ class VertigoProxyHandler(VertigoBaseHandler):
         # Dynamic binding of policies: using a Lua script that executes
         # a hgetall on the first matching key of a list
         """
-        key = 'mc_pipeline:00be261d2db3422e97693cdd91609c88/data/test.json'
+        key = 'mc_pipeline:00be261d2db3422e97693cdd91609c88:data:test.json'
         policy = {'get': '{"precedence": 1, \
                            "type": "clac", \
                            "input": {"policies": [ \
@@ -45,7 +45,7 @@ class VertigoProxyHandler(VertigoBaseHandler):
                            }'}
 
         for i in range(1, 1000):
-            key = 'mc_pipeline:00be261d2db3422e97693cdd91609c88/data/test{}.json'.format(i)
+            key = 'mc_pipeline:00be261d2db3422e97693cdd91609c88:data:test{}.json'.format(i)
             self.redis.hmset(key, policy)
         """
 
